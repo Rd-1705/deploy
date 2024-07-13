3a256f12aa69bbc82f27757bd27cf68405f5f0c1
Rd-1705 <akbarhidayat0812@gmail.com>
2024-07-13 18:55:34 +0700

    ali ganteng

---
+++ index.js
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express from "express";
+import  data  from "./main.js";
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json(data);
+})
+
+app.listen(3000, () => {
+    console.log("Server berhasil berjalan.");
+}); 
